a81c4b498dca16048e95b86139d48634fbd2b5cb
Aleksandr Malyshev <flesh.game@mail.ru>
2017-08-10 19:45:18 +0300

    init

---
+++ my-service/handler.py
@@ -0,0 +1,32 @@
+import json
+import sys
+import os
+
+
+here = os.path.dirname(os.path.realpath(__file__))
+sys.path.append(os.path.join(here, "./vendored"))
+from peewee import *
+from arrow import utcnow
+
+
+def hello(event, context):
+    body = {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "current time": utcnow().format(),
+        "input": event
+    }
+
+    response = {
+        "statusCode": 200,
+        "body": json.dumps(body)
+    }
+
+    return response
+
+    # Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    """
+    return {
+        "message": "Go Serverless v1.0! Your function executed successfully!",
+        "event": event
+    }
+    """
